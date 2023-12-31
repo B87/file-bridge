97e3adfe6b338731682c705b9afdd021d4f93a9c
Bernat Barroso <b.barroso87@gmail.com>
2023-12-31 17:07:27 +0100

    rename binary

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 
-go build -o filer .
+go build -o fileb .
 
 ## Create symklink to /usr/local/bin
-sudo ln -sf "$PWD"/filer /usr/local/bin/filer
-
-filer completion zsh > _filer
-
-sudo ln -sf "$PWD"/_filer "$(brew --prefix)"/share/zsh-autocomplete/_filer
+sudo ln -sf "$PWD"/fileb /usr/local/bin/fileb
